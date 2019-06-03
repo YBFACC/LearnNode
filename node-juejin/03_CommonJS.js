a77756cca6e6e03a60e72c479de17c1834d0154f
454447236@qq.com <454447236@qq.com>
2019-06-03 09:29:06 +0800

    fs
    文件管理

---
+++ node-juejin/03_CommonJS.js
@@ -0,0 +1,25 @@
+var http = require("http");
+
+var tools1 = require('./03_tool-add');
+
+http.createServer(function (req, res) {
+
+  res.writeHead(200, {
+    "Content-Type": "text/html;charset=UTF-8"
+  });
+
+  res.write('<h1 style="text-align:center">Hello NodeJS</h1>');
+  
+  console.log(tools1.add(1, 2, 3));
+  /**
+   * Console：
+   * 6
+   * 6
+   * 这里要记得 Node 运行过程中，它请求了两次，
+   * http://localhost:8080/ 为一次，
+   * http://localhost:8080/favicon.ico 为第二次
+   */
+  
+  res.end();
+
+}).listen(8080);
